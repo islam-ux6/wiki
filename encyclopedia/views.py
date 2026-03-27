9b264f3d416fd7f3f071e57975f99a0728ca195f
islam-ux6 <islammeretow@gmail.com>
2026-03-27 14:58:53 +0500

    test

---
+++ encyclopedia/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import markdown2
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def show_ent(request, title):
+    md_file = util.get_entry(title)
+    if md_file:
+        html = markdown2.markdown(md_file)
+        return render(request, "encyclopedia/show_entry.html", {
+            "html": html
+            })
+    else:
+        return HttpResponse("File Not Found")
